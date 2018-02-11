e66974a901dd15c5afb1a491c94266752f821ab4
Mike Dvorscak <mike.dvorscak@gmail.com>
2018-02-10 23:08:34 -0600

    Remove istanbul plugin from rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,11 @@
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
-import istanbul from 'rollup-plugin-istanbul';
 
 const pkg = require('./package.json');
 
 const plugins = [
   babel(babelrc())
 ];
-
-if (process.env.BUILD !== 'production') {
-  plugins.push(istanbul({
-    exclude: ['test/**/*', 'node_modules/**/*']
-  }));
-}
 
 export default {
   input: 'src/index.js',
